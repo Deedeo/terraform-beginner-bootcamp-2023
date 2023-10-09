4c2bdae36e1a2dd765074b05d9b6562f5d8f907d
ADEOLA DARAMOLA <daradeo4u@gmail.com>
2023-10-09 19:59:13 +0000

    #19 change structure of the root modules and tagging to bucket

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "Deebudapest"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-  }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-provider "random" {
-  # Configuration options
-}
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -46,7 +18,3 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 }
 
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  cloud {
+    organization = "Deebudapest"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+  }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+provider "random" {
+  # Configuration options
+}
